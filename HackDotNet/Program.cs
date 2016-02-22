c5b66abe8ffad11ebd74b9551dc05e8b613cdf3e
Anton Burkin <ramzzzay@gmail.com>
2016-02-23 02:59:00 +0300

    Create Repository

---
+++ HackDotNet/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Thx to ForNeVeR for idea and base implementation & nagg for jokes =)
+"(3:15:31 PM) nagg: Не дай бог придет такой наркоман на собес, ты у него спросишь чем отличается статик от не статик - а он: пфф... ничем. и у того и у того можно инстанс взять"
+*/
+using System;
+using System.Reflection;
+
+namespace HackDotNet
+{
+    class Program
+    {
+        static MethodInfo Allocate { get; } = typeof(RuntimeTypeHandle).GetMethod("Allocate", BindingFlags.NonPublic | BindingFlags.Static);
+        static object VoidType { get; } = Allocate.Invoke(null, new[] { typeof(void) });
+        private static void Main()
+        {
+            Console.WriteLine(VoidType.GetType()); // System.Void
+            //Console.WriteLine(voidType is Void);  // True
+            Console.WriteLine(ReturnMyVoid()); // System.Void
+        }
+
+        static object ReturnMyVoid() => VoidType;
+    }
+}
